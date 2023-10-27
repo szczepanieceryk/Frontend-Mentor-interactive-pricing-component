706b558a332a366f189672ea00de8bfa90294d46
eryk <e.szczepaniec@lightpixel.io>
2023-10-27 16:43:10 +0200

    add : color variables, page title, google styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">dzizła</div>;
+  return (
+    <div className="app-wrapper">
+      <div classNme="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/szczepanieceryk">Eryk Szczepaniec</a>.
+      </div>
+    </div>
+  );
 };
 
 export default App;
