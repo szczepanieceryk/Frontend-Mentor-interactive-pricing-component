737a00225504cce901eace5c50ea7325b03f71bb
eryk <e.szczepaniec@lightpixel.io>
2023-10-30 18:14:36 +0100

    add class names to Header elements and style them

--- src/Components/PricingComponent.js
+++ src/Components/PricingComponent.js
@@ -1,4 +1,13 @@
 import Form from "react-bootstrap/Form";
+
+// - 10K pageviews / $8 per month
+// - 50K pageviews / $12 per month
+// - 100K pageviews / $16 per month
+// - 500k pageviews / $24 per month
+// - 1M pageviews / $36 per month
+
+// If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+
 const PricingComponent = () => {
   return (
     <Form className="pricing-component">
